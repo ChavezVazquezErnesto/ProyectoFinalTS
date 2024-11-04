05be5e39fc6602c861dda917e6ebc44e6fb09551
ErnestoChav17 <98865119+ChavezVazquezErnesto@users.noreply.github.com>
2024-11-04 11:47:53 -0600

    Proyecto base
    Se creo el proyecto y se agregaron botones

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OnMainMenu;
+    public event Action OnItemsMenu;
+    public event Action OnArPosition;
+
+    public static GameManager instance;
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        OnMainMenu?.Invoke();
+        Debug.Log("Main Menu Activated");
+    }
+
+    public void ItemsMenu()
+    {
+        OnItemsMenu?.Invoke();
+        Debug.Log("Items Menu Activated");
+    }
+
+    public void ArPosition()
+    {
+        OnArPosition?.Invoke();
+        Debug.Log("Ar Position Activated");
+    }
+
+    public void CloseAPP()
+    {
+        Application.Quit();
+    }
+}
